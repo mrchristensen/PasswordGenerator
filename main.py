441a8f08460a86b7e2b818f25883a567fafee0e9
Matt Christensen <mr.christensen@gmail.com>
2021-11-01 16:32:46 -0600

    Initial implementation

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import sys
+import crypt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+changed_days = "30"
+min_days = "0"
+max_days = "99999"
+warn_days = "7"
+user_id = "1000"
+group_id = "1000"
+user_info = "GECOS"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_passwd(username):
+    ret = username + ":x:" + user_id + ":" + group_id + ":" + user_info + ":directory:shell11"
+
+    return ret
 
 
-# Press the green button in the gutter to run the script.
+def generate_shadow(username, password):
+    encrypted_password = crypt.crypt(password, crypt.METHOD_MD5)  # https://stackoverflow.com/questions/53416164/md5-hash-in-python
+
+    ret = username + ":" + encrypted_password + ":" + changed_days + ":" + min_days + ":" + max_days + ":" + warn_days + ":::"
+
+    return ret
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    username = sys.argv[1]
+    password = sys.argv[2]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(generate_passwd(username))
+    print(generate_shadow(username, password))
